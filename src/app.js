e612d14f0760da5b65ae6f6aeefe59e9412460ea
Andrew <ramettaandrewj@gmail.com>
2021-03-03 07:22:12 -0500

    add cors

--- src/app.js
+++ src/app.js
@@ -2,15 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const { CLIENT_ORIGIN } = require("./config");
 const helmet = require("helmet");
 const { NODE_ENV } = require("./config");
 
 const app = express();
 
-const morganOption = (NODE_ENV === "production" ? "tiny" : "common");
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(morgan(morganOption));
-app.use(cors());
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN,
+  })
+);
 app.use(helmet());
 
 app.get("/", (req, res) => {
