5a6413bcdbf02a9b2b5a3f7ed673dc13f5610b4c
Andrew <ramettaandrewj@gmail.com>
2021-03-06 13:08:47 -0500

    modified config and server

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
-const express = require("express");
+const knex = require("knex");
 const app = require("./app");
 
-const { PORT } = require("./config");
+const { PORT, DATABASE_URL } = require("./config");
 
-app.get("/api/*", (req, res) => {
-  res.json({ ok: true });
+const db = knex({
+  client: "pg",
+  connection: DATABASE_URL,
 });
+
+app.set("db", db);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
 });
-
-module.exports = { app };
