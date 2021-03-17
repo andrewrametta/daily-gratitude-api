33f1692605c18e38420153a630181585dd6c188d
Andrew <ramettaandrewj@gmail.com>
2021-03-16 21:39:46 -0400

    clean up app.js

--- src/app.js
+++ src/app.js
@@ -14,18 +14,6 @@ const morganOption = NODE_ENV === "production" ? "tiny" : "common";
 
 app.use(express.json());
 app.use(morgan(morganOption));
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-  );
-  next();
-});
-
 app.use(cors());
 app.use(helmet());
 
